d4481a387a21cb1da065989a5c364f3bd30596b0
YUSUF CAGRI DAL <yusufcagridal@gmail.com>
2023-07-02 21:46:55 +0300

    Initial commit

---
+++ src/hafta_01/Pratik_01_NotOrtalamasiHesapla.java
@@ -0,0 +1,16 @@
+package hafta_01;
+
+/*
+Ödev
+Aynı program içerisinde koşullu ifadeler kullanılarak, eğer kullanıcının ortalaması 60'dan büyük ise ekrana "Sınıfı Geçti" , küçük ise "Sınıfta Kaldı" yazsın.
+
+Not : If ve Else kullanılmayacak...
+ */
+
+public class Pratik_01_NotOrtalamasiHesapla {
+    public static void main(String[] args) {
+
+
+
+    }
+}

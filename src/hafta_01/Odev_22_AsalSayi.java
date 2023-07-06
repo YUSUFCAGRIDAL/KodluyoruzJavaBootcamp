65fb426c96287fe52e0092647da93c536e710abd
YUSUF CAGRI DAL <yusufcagridal@gmail.com>
2023-07-06 23:47:01 +0300

    files updated

--- src/hafta_01/Odev_22_AsalSayi.java
+++ src/hafta_01/Odev_22_AsalSayi.java
@@ -13,20 +13,21 @@ public class Odev_22_AsalSayi {
         System.out.println("1-100 arasindaki asal sayilar: ");
 
         //** 1 zaten asıl sayı olmadığından 2 den 100e kadar olan asıl kontrolü yapacağız**\\
-        for(i=2; i<=100; i++) {
+        for (i = 2; i <= 100; i++) {
             asal = true;
             //** Kontrole düşen sayı Asal olup olmadığını kontrol ediyoruz**\\
-            for(j=2; j<i; j++) {
+            for (j = 2; j < i; j++) {
                 //** Asal değil ise döngü burada son bulup diğer sayıya geçiyor**\\
-                if(i%j == 0) {
+                if (i % j == 0) {
                     asal = false;
                     break;
                 }
             }
             //** Asal ise yazdırıyoruz**\\
-            if(asal) {
+            if (asal) {
                 System.out.print(i + " ");
             }
         }
 
+    }
 }

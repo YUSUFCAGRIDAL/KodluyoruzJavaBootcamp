4c8a0c7300e9c26dad038d02f4b7fbf9f4f172ca
YUSUF CAGRI DAL <yusufcagridal@gmail.com>
2023-07-06 23:30:41 +0300

    files updated

---
+++ src/hafta_01/Odev_20_Mukemmel.java
@@ -0,0 +1,29 @@
+package hafta_01;
+
+import java.util.Scanner;
+
+public class Odev_20_Mukemmel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
+
+        int toplam = 0;
+
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {
+                toplam += i;
+            }
+        }
+
+        if (toplam == sayi) {
+            System.out.println(sayi + " Mükemmel sayıdır.");
+        } else {
+            System.out.println(sayi + " Mükemmel sayı değildir.");
+        }
+
+        scanner.close();
+    }
+
+}

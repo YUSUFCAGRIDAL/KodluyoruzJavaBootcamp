4f9f815f9d271d71acfd89dbb52c9c216f3f753f
YUSUF CAGRI DAL <yusufcagridal@gmail.com>
2023-07-06 21:44:44 +0300

    files updated

---
+++ src/hafta_01/Odev_11_ToplamProgrami.java
@@ -0,0 +1,32 @@
+package hafta_01;
+
+import java.util.Scanner;
+
+public class Odev_11_ToplamProgrami {
+import java.util.Scanner;
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            int toplam = 0;
+            int girilenSayi;
+
+            do {
+                System.out.print("Bir sayı giriniz (Programı sonlandırmak için 0 girin): ");
+                girilenSayi = scanner.nextInt();
+
+                if (girilenSayi % 2 == 0 && girilenSayi % 4 == 0) {
+                    toplam += girilenSayi;
+                }
+            } while (girilenSayi != 0);
+
+            System.out.println("Girilen değerlerden çift ve 4'ün katları olan sayıların toplamı: " + toplam);
+
+            scanner.close();
+        }
+    }
+
+
+
+
